d869722858f73418149974f9f4ab7df7842a1a5d
Mario Jackson <mario@capturemedia.ch>
2021-01-26 10:57:50 +0100

    Save and load sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/mariojackson/go-course/pkg/config"
 	"github.com/mariojackson/go-course/pkg/handlers"
@@ -11,9 +13,20 @@ import (
 )
 
 const portNumber = ":8080"
+var app config.AppConfig
+var session *scs.SessionManager
 
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	templateCache, err := render.CreateTemplateCache()
 	if err != nil {
